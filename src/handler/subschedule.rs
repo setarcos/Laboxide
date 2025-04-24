02f9c511b2c75e071e8bbb163f1c38fcf10a7254
Pluto Yang <yangyj.ee@gmail.com>
2025-04-24 09:37:56 +0800

    feat: permission check for subschedule

--- src/handler/subschedule.rs
+++ src/handler/subschedule.rs
@@ -1,16 +1,27 @@
+use actix_session::Session;
 use actix_web::{get, post, put, delete, web, HttpResponse, Responder};
 use serde_json::json;
 use sqlx::SqlitePool;
 
 use crate::db;
 use crate::models::SubSchedule;
+use crate::utils::check_course_perm;
 
 #[post("/subschedule")]
 pub async fn create_subschedule(
     db_pool: web::Data<SqlitePool>,
     item: web::Json<SubSchedule>,
+    session: Session,
 ) -> impl Responder {
-    match db::add_subschedule(&db_pool, item.into_inner()).await {
+    let sub = item.into_inner();
+    if let Ok(Some(schedule)) = db::get_schedule_by_id(&db_pool, sub.schedule_id).await {
+        if let Err(err) = check_course_perm(&db_pool, &session, schedule.course_id).await {
+            return err;
+        }
+    } else {
+        return HttpResponse::NotFound().json(json!({ "error": "Schedule not found" }));
+    }
+    match db::add_subschedule(&db_pool, sub).await {
         Ok(rec) => HttpResponse::Ok().json(rec),
         Err(e) => HttpResponse::InternalServerError().json(json!({ "error": e.to_string() })),
     }
@@ -56,8 +67,17 @@ pub async fn update_subschedule(
 pub async fn delete_subschedule(
     db_pool: web::Data<SqlitePool>,
     path: web::Path<i64>,
+    session: Session,
 ) -> impl Responder {
-    match db::delete_subschedule(&db_pool, path.into_inner()).await {
+    let sub_id = path.into_inner();
+    if let Ok(Some(schedule)) = db::get_schedule_by_id(&db_pool, sub_id).await {
+        if let Err(err) = check_course_perm(&db_pool, &session, schedule.course_id).await {
+            return err;
+        }
+    } else {
+        return HttpResponse::NotFound().json(json!({ "error": "Schedule not found" }));
+    }
+    match db::delete_subschedule(&db_pool, sub_id).await {
         Ok(true) => HttpResponse::Ok().json(json!({ "message": "SubSchedule deleted" })),
         Ok(false) => HttpResponse::NotFound().json(json!({ "error": "SubSchedule not found" })),
         Err(e) => HttpResponse::InternalServerError().json(json!({ "error": e.to_string() })),
